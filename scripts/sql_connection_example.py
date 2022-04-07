54f3b3ec3c6a5540608dc6926f25ac11440a7b0c
Jonathan Walker <>
2022-04-07 13:22:56 +0100

    sql connector example

--- scripts/sql_connection_example.py
+++ scripts/sql_connection_example.py
@@ -1,10 +1,12 @@
 from src.io.sql import SQLConnection
 
-conn = SQLConnection(
-    connection_name="hackathon-team-10:us-central1:ui-backend-test",
-    db="gdelt_sustainability"
-)
 
-result = conn.run_qry("select * from information_schema.tables")
+if __name__ == "__main__":
+    conn = SQLConnection(
+        connection_name="hackathon-team-10:us-central1:ui-backend-test",
+        db="gdelt_sustainability"
+    )
 
-print(result)
+    result = conn.run_qry("select * from information_schema.tables")
+
+    print(result)
